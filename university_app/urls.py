4e9371e02d50d2e1652833187e8c8976af55ba14
Monu Neolia <neoliamonu@gmail.com>
2023-02-11 15:47:22 +0530

    Generics APIView, ModelMixins, ModelSerialization, CRUD Urls

--- university_app/urls.py
+++ university_app/urls.py
@@ -5,27 +5,35 @@ from . import functionview
 from . import new_view
 from .functionview import *
 from .new_view import StudentView
+from . import generic_mixins_views
+from .generic_mixins_views import *
 
 # from .views import StudentListView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register('student', new_view.StudentView, basename='student')
-
+# router.register('student', new_view.StudentView, basename='student')
+# router.register('student', generic_mixins_views.StudentList, basename='stu')
 
 urlpatterns = [
     # path('success', views.success, name='success'),
     # path('', new_view.StudentView.as_view(), name='getAll'),
     # path('', include(router.urls)),
     # path('', StudentAPI.as_view()),
-    path('', functionview.getAll),
+    # path('', functionview.getAll),
     # path('api/student/<int:pk>', StudentAPI.as_view()),
-    path('api/<int:pk>', functionview.getStudent),
-    path('post', functionview.post),
-    path('put/<int:pk>', functionview.put),
-    path('patch/<int:pk>', functionview.patch),
-    path('delete/<int:pk>', functionview.delete)
+    path('', generic_mixins_views.StudentList.as_view()),
+    path('get/<int:pk>', generic_mixins_views.StudentRetrieve.as_view()),
+    path('post', generic_mixins_views.StudentCreate.as_view()),
+    path('patch/<int:pk>', generic_mixins_views.StudentPartial.as_view()),
+    path('put/<int:pk>', generic_mixins_views.StudentUpdate.as_view()),    
+    path('delete/<int:pk>', generic_mixins_views.StudentDelete.as_view())
+    # path('api/<int:pk>', functionview.getStudent),
+    # path('post', functionview.post),
+    # path('put/<int:pk>', functionview.put),
+    # path('patch/<int:pk>', functionview.patch),
+    # path('delete/<int:pk>', functionview.delete)
     # path('getStudent/<int:pk>', views.getStudent, name='getStudent'),
     # path('post', views.postStudent, name='post'),
     # path('students', views.get_all_student, name='getAll'),
